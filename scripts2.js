d6bb7af272cad43491eb40bf07691ebcbea922ec
TheNew000 <mahkatorocks@gmail.com>
2016-07-23 21:36:26 -0400

    cleaned up long code
    Made things more object oriented

--- scripts2.js
+++ scripts2.js
@@ -1,5 +1,6 @@
-var whosTurn = 1; //start off on player 1's turn
-
+var whosTurn = [];
+var someoneWon = false;
+var initialMoves = [4, 0, 8, 7, 3];
 var winners = [
 ["A1", "A2", "A3"],
 ["B1", "B2", "B3"],
@@ -10,7 +11,6 @@ var winners = [
 ["A3", "B3", "C3"],
 ["A3", "B2", "C1"]
 ];
-
 var moves = {
     A1: [["A3", "A2"], ["C3", "B2"], ["B1", "C1"]],
     A2: [["A3", "A1"], ["C2", "B2"]],
@@ -22,8 +22,6 @@ var moves = {
     B3: [["A3", "C3"], ["B1", "B2"]],
     B2: [["A3", "C1"], ["C3", "A1"], ["B3", "B1"], ["C2", "A2"]]
 }
-
-
 var player1 = {
     A1: 0,
     A2: 0,
@@ -35,7 +33,17 @@ var player1 = {
     C2: 0,
     C3: 0
 }
-
+var player2 = {
+    A1: 0,
+    A2: 0,
+    A3: 0,
+    B1: 0,
+    B2: 0,
+    B3: 0,
+    C1: 0,
+    C2: 0,
+    C3: 0
+}
 var cells = {
     A1: 0,
     A2: 0,
@@ -47,7 +55,6 @@ var cells = {
     C2: 0,
     C3: 0
 }
-
 var computer = {
     A1: 0,
     A2: 0,
@@ -59,48 +66,50 @@ var computer = {
     C2: 0,
     C3: 0
 }
-
-var someoneWon = false;
-var board1 = document.getElementById("game-wrapper");
-var board2 = document.getElementById("game2-wrapper");
-
-var A1 = document.getElementById('A1');
-var A2 = document.getElementById('A2');
-var A3 = document.getElementById('A3');
-var B1 = document.getElementById('B1');
-var B2 = document.getElementById('B2');
-var B3 = document.getElementById('B3');
-var C1 = document.getElementById('C1');
-var C2 = document.getElementById('C2');
-var C3 = document.getElementById('C3');
-
-
-
-// function createMemories(){
-//   for (var i = 0; i < array.length; ++i) {
-//       game[i] = [];
-//   }
-//   return game;
-// }
-
-var openCell = [];
-
-function compPlay(square){
-    if(whosTurn == 1 && (!player1[square.id] && !computer[square.id]){
-            square.innerHTML = 'X';
-            cells[square.id] = 1;
-            player1[square.id] = 1;
-            checkWin2(player1, 1);
-            whosTurn = 0;
-            checkMove();
-    }else{
-            square.innerHTML = '0';
-            computer[square.id] = 1;
-            cells[square.id] = 1;
-            checkWin2(computer, 0);
-            whosTurn = 1;
+function whichGame(player){
+    var elements = document.getElementsByClassName(box);
+    if(onePlayer){
+        whosTurn.push(1, 0);
+    }else(twoPlayer){
+        whosTurn.push(1, 2);
     }
 }
+
+
+function markSquare(square){
+    if(someoneWon){
+        console.log("Someone already won");
+    }else if(whosTurn[i] == 1 && (!player1[square.id] && !computer[square.id])){
+        whatHappens(square, player1);
+        if(whosTurn[1] == 0){
+            checkMove();
+        }
+    }else if (whosTurn == 0){
+        whatHappens(square, computer);
+    }else if(whosTurn == 2){
+        whatHappens(square, player2);
+    }else{
+        alert("Somethings already there!! No cheating!!");
+    }
+}
+
+function whatHappens(square, player){
+    if(player = player1){
+        square.innerHTML = 'X';
+        whosTurn[1];
+    }else{
+        square.innerHTML = 'O';
+        whosTurn[0]
+    }
+    cells[square.id] = 1;
+    player[square.id] = 1;
+    checkWin2(player, 1);
+    checkMove();
+
+}
+        
+
+
 
 function checkMove(){
     for (property in moves) {
@@ -108,16 +117,63 @@ function checkMove(){
             for (var i = 0; i < property.length; i++) {
                 var counter = 0;
                 for (var j = 0; j < property[i].length; j++) {
-                    if(cells[property[i][j]]){
+                    if(player1[property[i][j]]){
                         counter++;
-                        if(counter == property[i].length){
-                            compPlay(moves[i]);
+                        if(counter == property[i].length && !cells[i]){
+                            markSquare(moves[i]);
                         }
                     }else{
-
-                    } 
-                }
+                        for (var k = 0; k < initialMoves.length; k++) {
+                            if(!cells[k]){ 
+                                markSquare(moves[k]);
+                            }
+                        }
+                    }
+                }    
             }
         }
     }  
+}
+
+
+function checkWin(currentPlayer, whoJustMarked){
+    var rowCount = 0;
+    // Loops through the outer array
+    for (var i = 0; i < winners.length; i++) {
+        // Loop through each row's (inner array)
+        rowCount = 0;
+        for (var j = 0; j < winners[i].length; j++) {
+            if(currentPlayer[winners[i][j]]){
+                rowCount++;
+                if(rowCount == winners[i].length){
+                    gameOver(whoJustMarked, winners[i]);
+                    someoneWon = 1;
+                }
+            }else if(cells[i]){
+                message.innerHTML = "It's a tie!";
+            }
+        }
+    }
+}
+
+function gameOver(whoWon, winningCombo){
+    var message = document.getElementById('message');
+    if(whoWon == 1 || whoWon == 2){
+        message.innerHTML = "Congratulations Player " + whoWon + "!! You slayed with " + winningCombo.join(', ');
+    }else if(whoWon == 0){
+        message.innerHTML = "Unfortunately you were slain with this combo: " + winningCombo.join(', ');
+    }
+    for (var i = 0; i < winningCombo.length; i++) {
+        document.getElementById(winningCombo[i]).className += ' winner';
+    }
+
+}
+
+function restart(){
+    for (var i = 0; i < cells.length; i++) {
+        cells[i] = 0;
+        player1[i] = 0;
+        player2[i] = 0;
+        computer[i] = 0;
+    }
 }
