abf9cf99b768220ce697c0a6d4930260786290b3
TheNew000 <mahkatorocks@gmail.com>
2016-08-03 16:08:06 -0400

    rebuilt the game from the ground up using MINIMAX!!

--- scripts2.js
+++ scripts2.js
@@ -1,193 +1,169 @@
-var whosPlaying = [];
-var whosTurn = 'player1';
-var someoneWon = false;
-var initialMoves = ['B2', 'A1', 'C3', 'C2', 'B1'];
-var onePlayer = 0;
-var twoPlayer = 0;
-var player1 = [];
-var player2 = [];
-var computer = [];
-var winners = [
-["A1", "A2", "A3"],
-["B1", "B2", "B3"],
-["C1", "C2", "C3"],
-["A1", "B2", "C3"],
-["A1", "B1", "C1"],
-["A2", "B2", "C2"],
-["A3", "B3", "C3"],
-["A3", "B2", "C1"]
+var board = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
 ];
-var moves = {
-    A1: [["A3", "A2"], ["C3", "B2"], ["B1", "C1"]],
-    A2: [["A3", "A1"], ["C2", "B2"]],
-    A3: [["A1", "A2"], ["C1", "B2"], ["B3", "C3"]],
-    B1: [["B3", "B2"], ["A1", "C1"]],
-    B2: [["A3", "C1"], ["C3", "A1"], ["B3", "B1"], ["C2", "A2"]],
-    B3: [["A3", "C3"], ["B1", "B2"]],
-    C1: [["B2", "A3"], ["A1", "B1"], ["C2", "C3"]],
-    C2: [["A2", "B2"], ["C1", "C3"]],
-    C3: [["A1", "B2"], ["A3", "B3"], ["C2", "C1"]]
-};
-// var player1Grid = {
-//     A1: 0,
-//     A2: 0,
-//     A3: 0,
-//     B1: 0,
-//     B2: 0,
-//     B3: 0,
-//     C1: 0,
-//     C2: 0,
-//     C3: 0
-// };
-// var player2Grid = {
-//     A1: 0,
-//     A2: 0,
-//     A3: 0,
-//     B1: 0,
-//     B2: 0,
-//     B3: 0,
-//     C1: 0,
-//     C2: 0,
-//     C3: 0
-// };
-var cells = {
-    A1: 0,
-    A2: 0,
-    A3: 0,
-    B1: 0,
-    B2: 0,
-    B3: 0,
-    C1: 0,
-    C2: 0,
-    C3: 0
-};
-// var computer = {
-//     A1: 0,
-//     A2: 0,
-//     A3: 0,
-//     B1: 0,
-//     B2: 0,
-//     B3: 0,
-//     C1: 0,
-//     C2: 0,
-//     C3: 0
-// };
+var myMove = false;
+var numNodes = 0;
+var bools;
 
-function whichGame(player){
-    var elements = document.getElementsByClassName('box');
-    if(player == onePlayer){
-        whosPlaying.push('player1', 'computer');
+$(document).ready(function() {
+    $(".box").click(function() {
+        var cell = $(this).attr("id");
+        var row = parseInt(cell[cell.length - 2]);
+        var col = parseInt(cell[cell.length - 1]);
+        if (!myMove) {
+            board[row][col] = false;
+            myMove = true;
+            updateMove();
+            makeMove();
+        }
+    });
+    $("#restart").click(restartGame);
+});
+
+if (myMove) {
+    makeMove();
+}
+
+function updateMove() {
+    updateCells();
+    var winner = getWinner(board);
+
+    if(winner == 1){
+        $("#winner").text("Computer Won!");
+    }else if(winner == 0){
+        $("#winner").text("You Won!");
+    }else if(winner == -1){
+        $("#winner").text("Tie!");
     }else{
-        whosPlaying.push('player1', 'player2');
+        $("#winner").text("");
+    }
+    if(myMove){
+        $("#move").text("Computer's Move");
+    }else{
+        $("#move").text("Your Move!");
     }
 }
 
-function markSquare(square){
-    console.log(whosTurn);
-        if(whosTurn == 'player1' && cells[square.id] == 0){
-            whatHappens(square, 'player1');
-            if(whosPlaying[1] === 'computer'){
-                checkMove();
+function getWinner(board) {
+    // Check if someone won
+    bools = [true, false];
+    var allCellsNull = true;
+    for (var k = 0; k < bools.length; k++) {
+        var value = bools[k];
+        
+        // Check rows, columns, and diagonals
+        var diagonalComplete1 = true;
+        var diagonalComplete2 = true;
+        for (var i = 0; i < 3; i++) {
+            if (board[i][i] != value) {
+                diagonalComplete1 = false;
             }
-        }else if (whosTurn == 'computer'){
-            return whatHappens(square, computer);
-        }else if(whosTurn == player2){
-            return whatHappens(square, player2);
-        }else if(someoneWon){
-            return console.log("Someone already won");
-        }else{
-            return alert("Somethings already there!! No cheating!!");
-        }
-}
-
-function whatHappens(square, player){
-    if(player == 'player1'){
-        square.innerHTML = 'X';
-        cells[square.id] = 1;
-        player1.push(square.id);
-        checkWin(player1);
-        whosTurn = whosPlaying[1];
-    }else if(player == 'player2' || player == 'computer'){
-        console.log(square);
-        square.innerHTML = 'O';
-        cells[square.id] = 1;
-        if (player == 'player2'){
-            player2.push(square.id);
-            checkWin(player2);
-        }else{
-            computer.push(square.id);
-            checkWin(computer); 
-        }
-        whosTurn = whosPlaying[0];
-    }
-}
-
-function checkMove(){
-    for (var property in moves) {
-        if (moves.hasOwnProperty(property)) {
-            for (var i = 0; i < property.length; i++) {
-                for (var j = 0; j < property[i].length; j++) {
-                    if(player1.indexOf(property[i][j]) > -1){
-                        var newProperty = property.toString();
-                        if(player1.indexOf(property[i][0]) > -1 && player1.indexOf(property[i][1]) > -1 && cells[newProperty] == 0){
-                            var newSquare = document.getElementById(newProperty)
-                            return markSquare(newSquare);
-                        }
-                    }else{
-                        for (var k = 0; k < initialMoves.length; k++) {
-                            if(player1.indexOf(initialMoves[k]) < 0 && cells[initialMoves[k]] == 0){
-                                var newMove = initialMoves[k].toString();
-                                var finalMove = document.getElementById(newMove);
-                                console.log(finalMove);
-                                return whatHappens(finalMove, 'computer');
-                            }   
-                        }
-                    }
-                }    
+            if (board[2 - i][i] != value) {
+                diagonalComplete2 = false;
             }
-        }
-    }  
-}
-
-
-function checkWin(currentPlayer){
-    var rowCount = 0;
-    // Loops through the outer array
-    for (var i = 0; i < winners.length; i++) {
-        // Loop through each row's (inner array)
-        rowCount = 0;
-        for (var j = 0; j < winners[i].length; j++) {
-            if(currentPlayer[winners[i][j]]){
-                rowCount++;
-                if(rowCount == winners[i].length){
-                    gameOver(currentPlayer, winners[i]);
-                    someoneWon = 1;
+            var rowComplete = true;
+            var colComplete = true;
+            for (var j = 0; j < 3; j++) {
+                if (board[i][j] != value) {
+                    rowComplete = false;
                 }
-            }else if(cells[i]){
-                message.innerHTML = "It's a tie!";
+                if (board[j][i] != value) {
+                    colComplete = false;
+                }
+                if (board[i][j] == null) {
+                    allCellsNull = false;
+                }
+            }
+            if (rowComplete || colComplete) {
+                return value ? 1 : 0;
+            }
+        }
+        if (diagonalComplete1 || diagonalComplete2) {
+            return value ? 1 : 0;
+        }
+    }
+    if (allCellsNull) {
+        return -1;
+    }
+    return null;
+}
+    
+function updateCells() {
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+            console.dir("#cell-" + i + "" + j);
+            if(board[i][j] == false){
+                $("#cell-" + i + "" + j).text("X");
+            }else if(board[i][j] == true){
+                $("#cell-" + i + "" + j).text("O");
+            }else{
+                $("#cell-" + i + "" + j).text("");
             }
         }
     }
 }
 
-function gameOver(whoWon, winningCombo){
-    var message = document.getElementById('message');
-    if(whoWon == 1 || whoWon == 2){
-        message.innerHTML = "Congratulations Player " + whoWon + "!! You slayed with " + winningCombo.join(', ');
-    }else if(whoWon == 0){
-        message.innerHTML = "Unfortunately you were slain with this combo: " + winningCombo.join(', ');
-    }
-    for (var i = 0; i < winningCombo.length; i++) {
-        document.getElementById(winningCombo[i]).className += ' winner';
-    }
-
+function makeMove() {
+    board = minimaxMove(board);
+    myMove = false;
+    updateMove();
 }
 
-function restart(){
-    for (var i = 0; i < cells.length; i++) {
-        cells[i] = 0;
-        player1[i] = 0;
-        player2[i] = 0;
-        computer[i] = 0;
+function minimaxMove(board) {
+    numNodes = 0;
+    return miniMax(board, true)[1];
+}
+
+function miniMax(board, player) {
+    numNodes++;
+    var winner = getWinner(board);
+    if (winner != null) {
+        switch(winner) {
+            case 1:
+                // Computer wins
+                return [1, board]
+            case 0:
+                // You win
+                return [-1, board]
+            case -1:
+                // Tie
+                return [0, board];
+        }
+    } else {
+        // Next states
+        var nextVal = null;
+        var nextBoard = null;
+        
+        for (var i = 0; i < 3; i++) {
+            for (var j = 0; j < 3; j++) {
+                if (board[i][j] == null) {
+                    board[i][j] = player;
+                    var value = miniMax(board, !player)[0];
+                    if ((player && (nextVal == null || value > nextVal)) || (!player && (nextVal == null || value < nextVal))) {
+                        nextBoard = board.map(function(arr) {
+                            return arr.slice();
+                        });
+                        nextVal = value;
+                    }
+                    board[i][j] = null;
+                }
+            }
+        }
+        return [nextVal, nextBoard];
     }
 }
+
+function restartGame() {
+    board = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ];
+    myMove = false;
+    updateMove();
+}
+
+
+updateMove();
